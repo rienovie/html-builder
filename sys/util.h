4ebe05c970ccd04f4cf9c940c03498374b1b0cda
Vince <fisher.vince@gmail.com>
2023-12-05 19:39:07 -0500

    adding util functions

--- sys/util.h
+++ sys/util.h
@@ -1,8 +1,18 @@
 #pragma once
 
 #include <iostream>
+#include <string>
 
 namespace util {
+    int strToInt(std::string str);
+
     template <typename T>
     void qPrint(T output);
+
+    template <typename T>
+    void qPrint(T output0, T output1);
+
+    template <typename C, typename T>
+    void qPrint(C output0, T output1);
+
 }
